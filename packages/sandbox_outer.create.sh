352bca4ee6c52bbfb26e84c4859fa8c88464736c
Dmitry <dsagal@gmail.com>
2017-01-12 16:06:06 -0500

    Added nacl_python2 as a convenient way to run python in the sandbox

--- packages/sandbox_outer.create.sh
+++ packages/sandbox_outer.create.sh
@@ -71,13 +71,16 @@ git -C $NACL_DIR pull
 # Build from source Native Client's sel_ldr, the stand-alone "Secure ELF Loader"
 #----------------------------------------------------------------------
 echo "*** Building native_client's sel_ldr"
-NACL_SRC_TESTS="run_limited_file_access_test run_limited_file_access_ro_test"
-NACL_BUILD_RESULTS=$NACL_DIR/scons-out/opt-$OS_TYPE-$ARCHD/staging
 $NACL_DIR/scons -C "$NACL_DIR" platform=$ARCHD --mode="opt-$OS_TYPE" sel_ldr
+NACL_BUILD_RESULTS=$NACL_DIR/scons-out/opt-$OS_TYPE-$ARCHD/staging
 
-# Also build and run tests.
-echo "*** Building and running some native_client tests"
-$NACL_DIR/scons -C "$NACL_DIR" platform=$ARCHD $NACL_SRC_TESTS
+# Also build and run tests. This is just to check that the native_client repository has the
+# feature we want (limited file access), but isn't part of packaging, so disabled here.
+if [[ 1 -eq 0 ]]; then
+  echo "*** Building and running some native_client tests"
+  NACL_SRC_TESTS="run_limited_file_access_test run_limited_file_access_ro_test"
+  $NACL_DIR/scons -C "$NACL_DIR" platform=$ARCHD $NACL_SRC_TESTS
+fi
 
 #----------------------------------------------------------------------
 # Prepare the 'sandbox_outer' package with sel_ldr and convenience scripts.
@@ -86,9 +89,9 @@ $NACL_DIR/scons -C "$NACL_DIR" platform=$ARCHD $NACL_SRC_TESTS
 echo "*** Preparing files to package"
 mkdir -p $STAGE_DIR/bin
 
-copy_file $NACL_BUILD_RESULTS/sel_ldr   $STAGE_DIR/bin/
-copy_file $CHECKOUT_DIR/scripts/run     $STAGE_DIR/bin/
-#copy_file $DIR/scripts/nacl_python2     $STAGE_DIR/bin/
-#copy_file $DIR/scripts/nacl_python3     $STAGE_DIR/bin/
+copy_file $NACL_BUILD_RESULTS/sel_ldr         $STAGE_DIR/bin/
+copy_file $CHECKOUT_DIR/scripts/run           $STAGE_DIR/bin/
+copy_file $CHECKOUT_DIR/scripts/nacl_python2  $STAGE_DIR/bin/
+#copy_file $CHECKOUT_DIR/scripts/nacl_python3  $STAGE_DIR/bin/
 
 create_archive bin/
