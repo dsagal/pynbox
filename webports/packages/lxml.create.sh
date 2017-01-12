d2c5306d91b990d3052cce63bbaade4fd1c9d9bb
Dmitry <dsagal@gmail.com>
2017-01-12 00:04:28 -0500

    Create lxml.create.sh for lxml package, and factor out common pieces

---
+++ webports/packages/lxml.create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$(dirname $BASH_SOURCE[0])"
+source $DIR/util.sh
+
+extract libxml2_2.9.4
+extract libxslt_1.1.29
+extract zlib_1.2.8
+
+echo "Preparing files to package"
+copy_dir  --exclude '*.pyc' \
+          $NACL_SITE_PACKAGES/lxml                        $ROOT/lib/python2.7/site-packages/
+copy_file $NACL_SITE_PACKAGES/lxml-3.6.0-py2.7.egg-info   $ROOT/lib/python2.7/site-packages/
+
+create_archive
--- webports/packages/python.create.sh
+++ webports/packages/python.create.sh
@@ -1,96 +1,68 @@
 #!/bin/bash
 
-set -e -u
+DIR="$(dirname $BASH_SOURCE[0])"
+source $DIR/util.sh
 
-DEST_DIR=$1
-DEST_ARCHIVE="$DEST_DIR/python.tbz2"
-NACL_TOOLCHAIN_DIR=$NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl
-STRIP=$NACL_TOOLCHAIN_DIR/bin/strip
+extract corelibs_0.2
+extract gtest_1.7.0+
+extract ncurses_5.9
+extract readline_6.3
+extract zlib_1.2.8
+extract libtar_1.2.11
+extract nacl-spawn_0.1
+extract openssl_1.0.2e
+extract bzip2_1.0.6
+extract python_2.7.11
 
-xcopy_file() { rsync -Ltv --chmod=a-w "$@" | ( grep -Ev '^(sent |total size |$)' || true ); }
-xcopy_dir()  { rsync -rltv --safe-links --chmod=Fa-w "$@" | ( grep -Ev '^(sent |total size |$)' || true ); }
-
-copy_file() { rsync -Lt --chmod=a-w "$@" ; }
-copy_dir()  { rsync -rlt --safe-links --chmod=Fa-w "$@" ; }
-
-SUFFIX="_x86-64_glibc.tar.bz2"
-modules=(
-    corelibs_0.2
-    gtest_1.7.0+
-    ncurses_5.9
-    readline_6.3
-    zlib_1.2.8
-    libtar_1.2.11
-    nacl-spawn_0.1
-    openssl_1.0.2e
-    bzip2_1.0.6
-    python_2.7.11
-)
-
-rm -Rf packages/python/
-mkdir -p packages/python/payload
-for module in "${modules[@]}"; do
-  echo "Extracting out/packages/${module}${SUFFIX} to packages/python/"
-  tar -C packages/python/ -jxf "out/packages/${module}${SUFFIX}" payload/
-done
-
-cd packages/python
-mkdir -p root root/bin root/slib root/lib/python2.7
 echo "Preparing files to package"
 
 # Copy the python binary.
-copy_file payload/bin/python2.7.nexe          root/bin/
+copy_file $PAYLOAD/bin/python2.7.nexe          $ROOT/bin/
 
 # Include all the top-level python modules.
-copy_file payload/lib/python2.7/*.py          root/lib/python2.7/
+copy_file $PAYLOAD/lib/python2.7/*.py          $ROOT/lib/python2.7/
 
 # Include all the native modules that python needs.
-copy_dir payload/lib/python2.7/lib-dynload    root/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/lib-dynload   $ROOT/lib/python2.7/
 
 # To reduce the size of the sanboxed code, we skip a bunch of bigger python libraries that we
 # don't expect to work in the sandbox, as well as tests and some other unneeded files.
-copy_dir payload/lib/python2.7/sqlite3        root/lib/python2.7/
-copy_dir payload/lib/python2.7/logging        root/lib/python2.7/
-copy_dir payload/lib/python2.7/encodings      root/lib/python2.7/
-copy_dir payload/lib/python2.7/plat-nacl      root/lib/python2.7/
-copy_dir payload/lib/python2.7/importlib      root/lib/python2.7/
-copy_dir payload/lib/python2.7/xml            root/lib/python2.7/
-copy_dir payload/lib/python2.7/curses         root/lib/python2.7/
-copy_dir payload/lib/python2.7/site-packages  root/lib/python2.7/
-copy_dir payload/lib/python2.7/wsgiref        root/lib/python2.7/
-copy_dir payload/lib/python2.7/unittest       root/lib/python2.7/
-copy_dir --exclude '/test/' \
-         payload/lib/python2.7/email          root/lib/python2.7/
-copy_dir --exclude '/test/' \
-         payload/lib/python2.7/ctypes         root/lib/python2.7/
-copy_dir payload/lib/python2.7/hotshot        root/lib/python2.7/
-copy_dir payload/lib/python2.7/compiler       root/lib/python2.7/
-copy_dir --exclude '/tests/' \
-         payload/lib/python2.7/json           root/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/sqlite3       $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/logging       $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/encodings     $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/plat-nacl     $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/importlib     $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/xml           $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/curses        $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/site-packages $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/wsgiref       $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/unittest      $ROOT/lib/python2.7/
+copy_dir  --exclude 'test/' \
+          $PAYLOAD/lib/python2.7/email         $ROOT/lib/python2.7/
+copy_dir  --exclude 'test/' \
+          $PAYLOAD/lib/python2.7/ctypes        $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/hotshot       $ROOT/lib/python2.7/
+copy_dir  $PAYLOAD/lib/python2.7/compiler      $ROOT/lib/python2.7/
+copy_dir  --exclude 'tests/' \
+          $PAYLOAD/lib/python2.7/json          $ROOT/lib/python2.7/
 
 # Include the necessary shared libraries.
 #   This command shows the shared libraries that a binary or library requires:
 #   $NACL_TOOLCHAIN_DIR/bin/objdump -p bin/python2.7.nexe | grep NEEDED
 #   $NACL_TOOLCHAIN_DIR/bin/objdump -p lib/python2.7/lib-dynload/_ssl.so | grep NEEDED
-copy_file payload/lib/libc.so.11835d88        root/slib/
-copy_file payload/lib/libcrypt.so.11835d88    root/slib/
-copy_file payload/lib/libdl.so.11835d88       root/slib/
-copy_file payload/lib/libm.so.11835d88        root/slib/
-copy_file payload/lib/libpthread.so.11835d88  root/slib/
-copy_file payload/lib/libutil.so.11835d88     root/slib/
+copy_file $PAYLOAD/lib/libc.so.11835d88        $ROOT/slib/
+copy_file $PAYLOAD/lib/libcrypt.so.11835d88    $ROOT/slib/
+copy_file $PAYLOAD/lib/libdl.so.11835d88       $ROOT/slib/
+copy_file $PAYLOAD/lib/libm.so.11835d88        $ROOT/slib/
+copy_file $PAYLOAD/lib/libpthread.so.11835d88  $ROOT/slib/
+copy_file $PAYLOAD/lib/libutil.so.11835d88     $ROOT/slib/
 
-copy_file payload/lib/libbz2.so.1.0           root/slib/
-copy_file payload/lib/libcrypto.so.1.0.0      root/slib/
-copy_file payload/lib/libncurses.so.5         root/slib/
-copy_file payload/lib/libpanel.so.5           root/slib/
-copy_file payload/lib/libreadline.so          root/slib/
-copy_file payload/lib/libssl.so.1.0.0         root/slib/
-copy_file payload/lib/libz.so.1               root/slib/
+copy_file $PAYLOAD/lib/libbz2.so.1.0           $ROOT/slib/
+copy_file $PAYLOAD/lib/libcrypto.so.1.0.0      $ROOT/slib/
+copy_file $PAYLOAD/lib/libncurses.so.5         $ROOT/slib/
+copy_file $PAYLOAD/lib/libpanel.so.5           $ROOT/slib/
+copy_file $PAYLOAD/lib/libreadline.so          $ROOT/slib/
+copy_file $PAYLOAD/lib/libssl.so.1.0.0         $ROOT/slib/
+copy_file $PAYLOAD/lib/libz.so.1               $ROOT/slib/
 
-# We can reduce sizes substantially by stripping binaries and shared libraries.
-$STRIP root/bin/*.nexe
-$STRIP root/slib/*.so*
-$STRIP root/lib/python2.7/lib-dynload/*.so*
-
-echo "Creating archive $DEST_ARCHIVE"
-tar -jcf $DEST_ARCHIVE root/
+create_archive
